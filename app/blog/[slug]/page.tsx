152dd90c619214f8771b185baf5daa15571a9964
pqhung3007 <hungpham307@gmail.com>
2024-10-06 11:27:23 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default function Page({ params }: Params) {
   return (
     <>
       <h1>Slug: {params.slug}</h1>
-      <p>Quang Hung Pham</p>
+      <p>Quang Hung</p>
       <p>Welcome to Hanoi</p>
     </>
   )
